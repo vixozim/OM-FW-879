973c0ee8e5f19ed3db8ff9724293633b7f4cce18
Vixoz <vixozim@gmail.com>
2023-02-17 16:19:39 -0300

    mapa

--- js/main.js
+++ js/main.js
@@ -74,3 +74,25 @@ const getDatePickerTitle = elem => {
       title: getDatePickerTitle(elem)
     });
   }
+
+  
+        
+        
+$(document).ready(function() {
+
+    $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+}, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+        $(this).text(Math.ceil(now));
+    }
+});
+});
+
+});  
+
+
+
